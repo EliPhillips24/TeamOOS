c73367a22b18d729ef88c064507d675ba1d46458
kingfisheii <132377565+kingfisheii@users.noreply.github.com>
2023-09-21 08:32:42 -0400

    NewCodeA

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       // System.out.println("Hello world!");
+
+        Ref ref1 = new Ref();
+        ref1.name = "Mr. Big";
+        ref1.name();
+
+        ref1.sport = " Soccer";
+        ref1.sport();
+        ref1.year = 10;
+        ref1.year();
+
+
+        Ref ref2 = new Ref();
+        ref2.name = "Mr. Small";
+        ref2.name();
+
+        ref2.sport = " Soccer";
+        ref2.sport();
+        ref2.year = 7;
+        ref2.year();
+
     }
 
     Player player1 = new Player();
-
 
 
 
