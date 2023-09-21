c155256ecf599d6c9f91d4e3e69c1a09f81eea98
brandonkay <bkay24@scienceleadership.org>
2023-09-21 08:52:27 -0400

    Hello

--- src/Main.java
+++ src/Main.java
@@ -32,10 +32,10 @@ public class Main {
         Player player2 = new Player();
         player2.name = "Price Watt";
         player2.Name();
-        player2.sport = "Soccer";
-        player2.Sport();
         player2.team = "Danville Ponchos";
         player2.Team();
+        player2.sport = "Soccer";
+        player2.Sport();
 
 
     }
