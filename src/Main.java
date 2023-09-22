827183e784e98ae01f5910839ed0d9c208c8a4fe
brandonkay <bkay24@scienceleadership.org>
2023-09-22 10:16:20 -0400

    Hello

--- src/Main.java
+++ src/Main.java
@@ -39,7 +39,7 @@ public class Main {
 
         coach1.Name = "Brad Chase";
         coach1.introduce_self();
-        coach1.Team = "Bronvill Weasels";
+        coach1.Team = team1;
         coach1.introduce_team();
         coach1.Sport = "Soccer Tournament";
         coach1.declare_win();
@@ -47,7 +47,7 @@ public class Main {
 
         coach2.Name = "Justin Netenhao";
         coach2.introduce_self();
-        coach2.Team = "Danville Ponchos";
+        coach2.Team = team2;
         coach2.introduce_team();
         coach2.Sport = "Bronvill Weasels";
         coach2.declare_win2();
